94b49d7c847538646a2c160f6e5bc0ad67c8b693
MarcosJRuiz <marcosruiz.jt@gmail.com>
2023-05-29 19:42:16 -0300

    branch ruiz commit: file do-while.js

---
+++ do-while.js
@@ -0,0 +1,10 @@
+﻿// Executar no terminal o comando: node while.js
+
+let index = 0
+
+do {
+    console.log(`Valor comparado é true: ${index}`)
+    index = index + 1
+} while (index <= 3)
+
+console.log(`Valor comparado é false: ${index}`)
