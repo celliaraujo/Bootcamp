58ce3b75d02c056c39eec897fd2a0541d7f8a589
MarcosJRuiz <marcosruiz.jt@gmail.com>
2023-05-29 18:49:49 -0300

    git init with file C2valores.js

---
+++ c2valores.js
@@ -0,0 +1,14 @@
+﻿// Executar no terminal o comando: node c2valores.js
+
+var a =5
+var b = 3
+
+console.log("var a= "+a)
+console.log("var b= "+b)
+
+console.log("a==b : ".concat(a==b))
+console.log("a<>b : ".concat(a!=b))
+console.log("a>b : ".concat(a>b))
+console.log("a<b : ".concat(a<b))
+console.log("a>=b : ".concat(a>=b))
+console.log("a<=b : ".concat(a<=b))
